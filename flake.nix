cdf18eddd8bdf9d0ff989efb968a1be110257649
Peter Burns <rictic@gmail.com>
2025-05-28 16:26:46 -0700

    Add Nix flake and workflow

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Hypnos Discord bot";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "hypnos";
+          version = "1.2.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          buildInputs = [ pkgs.openssl ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ rustc cargo openssl pkg-config ];
+        };
+      });
+}
